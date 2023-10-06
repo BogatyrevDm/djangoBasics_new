4b93ce8b91d3c469fefb5e656b67faf1f7f6620a
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-10-06 07:30:48 +0300

    static site with Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse
-from django.views.generic import View
-
-
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Hello world")
+from django.views.generic import TemplateView
 
 
 
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
 
-def check_kwargs(request, **kwargs):
-    return HttpResponse(f'kwargs<br>{kwargs}')
+class CoursesListPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
